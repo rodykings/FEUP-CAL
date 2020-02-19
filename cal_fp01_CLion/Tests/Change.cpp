9c606257be6e3ea87690df41842e35648a15ad26
rodykings <rodrigocreis.12@gmail.com>
2020-02-19 08:58:49 +0000

    Added first exercises

---
+++ cal_fp01_CLion/Tests/Change.cpp
@@ -0,0 +1,12 @@
+/*
+ * Change.cpp
+ */
+
+#include "Change.h"
+
+string calcChange(int m, int numCoins, int *coinValues)
+{
+    return "";
+}
+
+
---
+++ cal_fp01_CLion/Tests/Factorial.cpp
@@ -0,0 +1,16 @@
+/*
+ * Factorial.cpp
+ */
+
+#include "Factorial.h"
+
+int factorialRecurs(int n)
+{
+	return 1;
+
+}
+
+int factorialDinam(int n)
+{
+	return 1;
+}
